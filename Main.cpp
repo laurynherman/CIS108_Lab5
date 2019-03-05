6bc4c21081aa8aa6de3429e2687ceaa12af150d5
Lauryn Herman <46700787+laurynherman@users.noreply.github.com>
2019-03-04 18:34:42 -0800

    Add files via upload

---
+++ Main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std; 
+
+int main()
+
+{
+	cout << "Please enter song title: ";
+	cin >> song_title;
+
+	cout << "Please enter the name of artist: ";
+	cin >> song_artist;
+
+	cout << "Please enter song album: ";
+	cin >> song_album;
+
+	cout << "Please enter song track number: ";
+	cin >> song_track_number;
+
+
+}
---
+++ Song.h
@@ -0,0 +1,11 @@
+#pragma once
+
+typedef struct _Song
+{
+	char song_title;
+	char song_author;
+	char song_album;
+	uint32_t song_track_num;
+	uint32_t song_year;
+
+};
