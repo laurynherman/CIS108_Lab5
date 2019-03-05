667c833f000898d83886a17876c07807991ce7dd
Lauryn Herman <46700787+laurynherman@users.noreply.github.com>
2019-03-04 20:51:47 -0800

    Add files via upload

--- Main.cpp
+++ Main.cpp
@@ -1,22 +1,39 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include "MusicDB.h"
+#include "Song.h"
 
 using namespace std; 
 
 int main()
 
 {
-	cout << "Please enter song title: ";
+	string str;
+	
+	string song_title;
+	cout << "Please enter song title: \n ";
+	getline(cin, str);
+	
 	cin >> song_title;
 
+	string song_artist;
 	cout << "Please enter the name of artist: ";
+	getline(cin, str);
+
 	cin >> song_artist;
 
+	string song_album;
 	cout << "Please enter song album: ";
+	getline(cin, str);
+
 	cin >> song_album;
 
+	int song_track_number;
+
 	cout << "Please enter song track number: ";
+	getline(cin, str);
+
 	cin >> song_track_number;
 
 
